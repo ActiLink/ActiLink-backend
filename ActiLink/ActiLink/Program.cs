bd995a2bb34372c41b333498429342071fc5eeff
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-03-12 11:22:08 +0100

    Add Swagger

--- ActiLink/ActiLink/Program.cs
+++ ActiLink/ActiLink/Program.cs
@@ -6,12 +6,27 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    /*
+     Does not work with Docker container
+    */
+    //app.UseSwaggerUI(options =>
+    //{
+    //    options.SwaggerEndpoint("/openapi/v1.json", "ActiLink API");
+    //});
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
