147e768777accda6a8fd0fb3d136932874e4b19e
Wojciech Łapan <Wojtekl770@interia.pl>
2025-04-02 16:56:56 +0200

    Klasa Event i EventDto

---
+++ ActiLink/ActiLink/DTOs/EventDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ActiLink.DTOs
+{
+    /// <summary>
+    /// Data transfer object for an event
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="Date"></param>
+    /// <param name="Location"></param>
+    /// <param name="OrganizerId"></param>
+    /// <param name="HobbyId"></param>
+    public record EventDto(string Id, string Name, DateTime Date, string Location, string OrganizerId, string HobbyId)
+    {
+        public EventDto() : this(default!, default!, default!, default!, default!, default!) { }
+    }
+}
---
+++ ActiLink/ActiLink/Model/Event.cs
@@ -0,0 +1,26 @@
+﻿namespace ActiLink.Model
+{
+    /// <summary>
+    /// Represents an event
+    /// </summary>
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public User Organizer { get; set; }
+        public Hobby Type { get; set; }
+        public ICollection<User> Participants { get; } = new List<User>();
+
+        public Event(string id, string name, DateTime date, string location, User organizer, Hobby type)
+        {
+            Id = id;
+            Name = name;
+            Date = date;
+            Location = location;
+            Organizer = organizer;
+            Type = type;
+        }
+    }
+}
