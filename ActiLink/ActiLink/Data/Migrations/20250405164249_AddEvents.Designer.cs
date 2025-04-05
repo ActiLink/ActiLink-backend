3afe123367f2d36262e57becd13ab01b777c98f0
Wojciech Łapan <Wojtekl770@interia.pl>
2025-04-05 18:46:50 +0200

    cos

--- ActiLink/ActiLink/Data/Migrations/20250405164249_AddEvents.Designer.cs
+++ ActiLink/ActiLink/Data/Migrations/20250405164249_AddEvents.Designer.cs
@@ -9,10 +9,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace ActiLink.Migrations
+namespace ActiLink.Data.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    [Migration("20250402202423_AddEvents")]
+    [Migration("20250405164249_AddEvents")]
     partial class AddEvents
     {
         /// <inheritdoc />
@@ -44,9 +44,6 @@ namespace ActiLink.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<string>("OrganizerId1")
-                        .HasColumnType("nvarchar(450)");
-
                     b.Property<decimal>("Price")
                         .HasColumnType("decimal(18,2)");
 
@@ -56,8 +53,6 @@ namespace ActiLink.Migrations
                     b.HasKey("Id");
 
                     b.HasIndex("OrganizerId");
-
-                    b.HasIndex("OrganizerId1");
 
                     b.ToTable("Events");
                 });
@@ -315,28 +310,24 @@ namespace ActiLink.Migrations
 
             modelBuilder.Entity("ActiLink.Model.Event", b =>
                 {
-                    b.HasOne("ActiLink.Model.Organizer", null)
-                        .WithMany()
+                    b.HasOne("ActiLink.Model.Organizer", "Organizer")
+                        .WithMany("Events")
                         .HasForeignKey("OrganizerId")
                         .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
-
-                    b.HasOne("ActiLink.Model.Organizer", null)
-                        .WithMany("Events")
-                        .HasForeignKey("OrganizerId1");
 
                     b.OwnsOne("ActiLink.Model.Location", "Location", b1 =>
                         {
                             b1.Property<Guid>("EventId")
                                 .HasColumnType("uniqueidentifier");
 
-                            b1.Property<int>("Height")
-                                .HasColumnType("int")
-                                .HasColumnName("Location_Latitude");
+                            b1.Property<double>("Latitude")
+                                .HasColumnType("float")
+                                .HasColumnName("Latitude");
 
-                            b1.Property<int>("Width")
-                                .HasColumnType("int")
-                                .HasColumnName("Location_Longitude");
+                            b1.Property<double>("Longitude")
+                                .HasColumnType("float")
+                                .HasColumnName("Longitude");
 
                             b1.HasKey("EventId");
 
@@ -348,6 +339,8 @@ namespace ActiLink.Migrations
 
                     b.Navigation("Location")
                         .IsRequired();
+
+                    b.Navigation("Organizer");
                 });
 
             modelBuilder.Entity("ActiLink.Model.Hobby", b =>
---
+++ ActiLink/ActiLink/Data/Migrations/20250405164249_AddEvents.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActiLink.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEvents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId1",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_OrganizerId1",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Location_Latitude",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Location_Longitude",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizerId1",
+                table: "Events");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Events",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Events",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Location_Latitude",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Location_Longitude",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizerId1",
+                table: "Events",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganizerId1",
+                table: "Events",
+                column: "OrganizerId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_AspNetUsers_OrganizerId1",
+                table: "Events",
+                column: "OrganizerId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
