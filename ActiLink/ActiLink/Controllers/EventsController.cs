be2475939dfdc6ccb5c27d8ec530d106b038a8af
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-06 13:09:43 +0200

    Add empty service and controller for events

---
+++ ActiLink/ActiLink/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using ActiLink.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiLink.Controllers
+{
+    /// <summary>
+    /// Controller for managing events/activities.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        private readonly EventService eventService;
+        private readonly ILogger<UsersController> _logger;
+        private readonly IMapper _mapper;
+
+        public EventsController(ILogger<UsersController> logger, EventService eventService, IMapper mapper)
+        {
+            this.eventService = eventService;
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
