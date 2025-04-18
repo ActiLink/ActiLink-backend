af0c8fea6c8d36f5c9d1897a99da5799cae7edf7
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-18 13:52:35 +0200

    Add  classes related to business clients

---
+++ ActiLink/ActiLink/Organizers/BusinessClients/BusinessClient.cs
@@ -0,0 +1,14 @@
+﻿namespace ActiLink.Organizers.BusinessClients
+{
+    public class BusinessClient : Organizer
+    {
+        public BusinessClient(string username, string email, string taxId) : base(username, email)
+        {
+            TaxId = taxId;
+        }
+
+        public string TaxId { get; set; }
+
+    }
+
+}
---
+++ ActiLink/ActiLink/Organizers/BusinessClients/DTOs/NewBusinessClientDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ActiLink.Organizers.BusinessClients.DTOs
+{
+    /// <summary>
+    /// Data transfer object for creating a new business client
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Email"></param>
+    /// <param name="Password"></param>
+    /// <param name="TaxId"></param>
+    public record NewBusinessClientDto(string Name, string Email, string Password, string TaxId);
+}
