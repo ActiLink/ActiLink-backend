eb552f7031740f4180d9258d2d1970ee7d6ea134
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-18 14:38:40 +0200

    Fix BusinessClient constructor for EF binding

--- ActiLink/ActiLink/Organizers/BusinessClients/BusinessClient.cs
+++ ActiLink/ActiLink/Organizers/BusinessClients/BusinessClient.cs
@@ -2,12 +2,14 @@
 {
     public class BusinessClient : Organizer
     {
-        public BusinessClient(string username, string email, string taxId) : base(username, email)
+
+        //public BusinessClient(string username, string email) : base(username, email) { }
+        public BusinessClient(string userName, string email, string taxId) : base(userName, email)
         {
             TaxId = taxId;
         }
 
-        public string TaxId { get; set; }
+        public string TaxId { get; set; } = string.Empty;
 
     }
 
