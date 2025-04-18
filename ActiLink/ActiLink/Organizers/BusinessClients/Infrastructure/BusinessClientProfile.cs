4a45bbd7bde95662cc90b9d7bbc11482e744a1a6
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-18 13:56:25 +0200

    Add mapping between BusinessClient and BusinessClientDto

---
+++ ActiLink/ActiLink/Organizers/BusinessClients/Infrastructure/BusinessClientProfile.cs
@@ -0,0 +1,14 @@
+﻿using ActiLink.Organizers.BusinessClients.DTOs;
+using AutoMapper;
+
+namespace ActiLink.Organizers.BusinessClients.Infrastructure
+{
+    public class BusinessClientProfile : Profile
+    {
+        public BusinessClientProfile()
+        {            
+            CreateMap<BusinessClient, BusinessClientDto>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.UserName));
+        }
+    }
+}
