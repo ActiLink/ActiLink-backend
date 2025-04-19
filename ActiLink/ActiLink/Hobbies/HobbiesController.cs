7ab4a8598d2f378113b82650d0e22aca855454a2
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-19 14:33:54 +0200

    Add attributes to HobbiesController.GetHobbyByIdAsync()

--- ActiLink/ActiLink/Hobbies/HobbiesController.cs
+++ ActiLink/ActiLink/Hobbies/HobbiesController.cs
@@ -50,6 +50,9 @@ namespace ActiLink.Hobbies
         /// with the <see cref="HobbyDto"/> object or an error response if the hobby was not found.
         /// </returns>
         [HttpGet("{id}")]
+        [Authorize]
+        [ProducesResponseType<HobbyDto>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> GetHobbyByIdAsync(Guid id)
         {
             var hobby = await _hobbyService.GetHobbyByIdAsync(id);
