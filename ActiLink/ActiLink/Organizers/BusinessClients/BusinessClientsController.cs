300ddf127de568dcddc88f356495903cd061f134
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-19 12:04:47 +0200

    Add a service and a controller for business clients

---
+++ ActiLink/ActiLink/Organizers/BusinessClients/BusinessClientsController.cs
@@ -0,0 +1,107 @@
+﻿using ActiLink.Organizers.BusinessClients.DTOs;
+using ActiLink.Organizers.BusinessClients.Service;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiLink.Organizers.BusinessClients
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BusinessClientsController : ControllerBase
+    {
+        private readonly IBusinessClientService _businessClientService;
+        private readonly ILogger<BusinessClientsController> _logger;
+        private readonly IMapper _mapper;
+
+        public BusinessClientsController(ILogger<BusinessClientsController> logger, IBusinessClientService businessClientService, IMapper mapper)
+        {
+            _businessClientService = businessClientService;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Creates a new business client with the specified details in the request body.
+        /// </summary>
+        /// <param name="newBusinessClientDto">The data transfer object containing the new business client's details.</param>
+        /// <returns>Returns a CreatedAtAction result with the created business client's details or an error response.</returns>
+        [HttpPost("register")]
+        [ProducesResponseType<BusinessClientDto>(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateBusinessClientAsync([FromBody] NewBusinessClientDto newBusinessClientDto)
+        {
+            BusinessClient? businessClient = null;
+
+            try
+            {
+                var (username, email, password, taxId) = newBusinessClientDto;
+                _logger.LogInformation("Creating business client {username} with email {email}", username, email);
+
+                var result = await _businessClientService.CreateBusinessClientAsync(username, email, password, taxId);
+
+                if (!result.Succeeded)
+                {
+                    _logger.LogWarning("Business client registration failed: {errors}", result.Errors);
+                    return BadRequest(result.Errors);
+                }
+
+                businessClient = result.Data!;
+                _logger.LogInformation("Business client {businessClientId} created successfully", businessClient.Id);
+                return CreatedAtAction(nameof(GetBusinessClientByIdAsync), new { id = businessClient.Id }, _mapper.Map<BusinessClientDto>(businessClient));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred");
+
+                if (businessClient is not null)
+                    _logger.LogError("Business client {businessClientId} creation failed", businessClient.Id);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred");
+            }
+        }
+
+        /// <summary>
+        /// Fetches all business clients.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, 
+        /// containing an <see cref="IEnumerable{T}"/> of <see cref="BusinessClientDto"/> objects.
+        /// </returns>
+        [HttpGet]
+        [Authorize]
+        [ProducesResponseType<IEnumerable<BusinessClientDto>>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IEnumerable<BusinessClientDto>> GetBusinessClientsAsync()
+        {
+            _logger.LogInformation("Fetching all business clients");
+            var businessClients = await _businessClientService.GetBusinessClientsAsync();
+            _logger.LogInformation("Returning {businessClientsCount} business clients", businessClients.Count());
+            return _mapper.Map<IEnumerable<BusinessClientDto>>(businessClients);
+        }
+
+
+        [HttpGet("{id}")]
+        [Authorize]
+        [ActionName(nameof(GetBusinessClientByIdAsync))]
+        [ProducesResponseType<BusinessClientDto>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetBusinessClientByIdAsync([FromRoute] string id)
+        {
+            _logger.LogInformation("Fetching business client with ID {id}", id);
+            var businessClient = await _businessClientService.GetBusinessClientByIdAsync(id);
+
+            if (businessClient is null)
+            {
+                _logger.LogWarning("Business client with ID {id} not found", id);
+                return NotFound();
+            }
+
+            _logger.LogInformation("Business client with ID {id} found", id);
+            return Ok(_mapper.Map<BusinessClientDto>(businessClient));
+        }
+    }
+}
---
+++ ActiLink/ActiLink/Organizers/BusinessClients/Service/BusinessClientService.cs
@@ -0,0 +1,78 @@
+﻿using ActiLink.Shared.Repositories;
+using ActiLink.Shared.ServiceUtils;
+using Microsoft.AspNetCore.Identity;
+
+namespace ActiLink.Organizers.BusinessClients.Service
+{
+    public class BusinessClientService : IBusinessClientService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<Organizer> _userManager;
+
+        public BusinessClientService(IUnitOfWork unitOfWork, UserManager<Organizer> userManager)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+
+        /// <summary>
+        /// Creates a new business client with the specified <paramref name="username"/>, <paramref name="email"/>, <paramref name="password"/> and <paramref name="taxId"/>.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="taxId"></param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing the <see cref="GenericServiceResult{T}"/> of the operation
+        /// with the created <see cref="BusinessClient"/> object or null and error messages if the creation failed.
+        /// </returns>
+        public async Task<GenericServiceResult<BusinessClient>> CreateBusinessClientAsync(string username, string email, string password, string taxId)
+        {
+            var businessClient = new BusinessClient(username, email, taxId);
+            var result = await _userManager.CreateAsync(businessClient, password);
+
+            return result.Succeeded ? GenericServiceResult<BusinessClient>.Success(businessClient) : GenericServiceResult<BusinessClient>.Failure(result.Errors.Select(e => e.Description));
+        }
+
+        /// <summary>
+        /// Retrieves all business clients from the database.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing an <see cref="IEnumerable{T}"/> of <see cref="BusinessClient"/> objects.
+        /// </returns>
+        public async Task<IEnumerable<BusinessClient>> GetBusinessClientsAsync()
+        {
+            return await _unitOfWork.BusinessClientRepository.GetAllAsync();
+        }
+
+
+        /// <summary>
+        /// Finds and returns a business client, if any, by the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing the <see cref="BusinessClient"/> object if found, or null.
+        /// </returns>
+        public async Task<BusinessClient?> GetBusinessClientByIdAsync(string id)
+        {
+            return await _userManager.FindByIdAsync(id) as BusinessClient;
+        }
+
+        /// <summary>
+        /// Deletes the specified <paramref name="businessClient"/> from the database if exists.
+        /// </summary>
+        /// <param name="businessClient"></param>
+        /// <returns>
+        /// The <see cref="Task"/> that represents the asynchronous operation, containing a <see cref="ServiceResult"/> indicating success or failure.
+        /// </returns>
+        public async Task<ServiceResult> DeleteBusinessClientAsync(BusinessClient businessClient)
+        {
+            var result = await _userManager.DeleteAsync(businessClient);
+
+            return result.Succeeded
+                ? ServiceResult.Success()
+                : ServiceResult.Failure(result.Errors.Select(e => e.Description));
+        }
+    }
+}
