9217a9e46ad720968d008dcbcc014c01d40c6580
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-04-19 10:37:07 +0200

    Add a repository in UoW for BusinessClient

--- ActiLink/ActiLink/Shared/Repositories/UnitOfWork.cs
+++ ActiLink/ActiLink/Shared/Repositories/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using ActiLink.Events;
 using ActiLink.Hobbies;
 using ActiLink.Organizers.Authentication;
+using ActiLink.Organizers.BusinessClients;
 using ActiLink.Organizers.Users;
 
 namespace ActiLink.Shared.Repositories
@@ -9,6 +10,7 @@ namespace ActiLink.Shared.Repositories
     {
         private readonly ApiContext _context;
         private IRepository<User>? _userRepository;
+        private IRepository<BusinessClient>? _businessClientRepository;
         private IRepository<Event>? _eventRepository;
         private IRepository<Hobby>? _hobbyRepository;
         private IRepository<RefreshToken>? _refreshTokenRepository;
@@ -19,6 +21,7 @@ namespace ActiLink.Shared.Repositories
         }
 
         public IRepository<User> UserRepository => _userRepository ??= new Repository<User>(_context);
+        public IRepository<BusinessClient> BusinessClientRepository => _businessClientRepository ??= new Repository<BusinessClient>(_context);
         public IRepository<Event> EventRepository => _eventRepository ??= new Repository<Event>(_context);
         public IRepository<Hobby> HobbyRepository => _hobbyRepository ??= new Repository<Hobby>(_context);
         public IRepository<RefreshToken> RefreshTokenRepository => _refreshTokenRepository ??= new Repository<RefreshToken>(_context);
