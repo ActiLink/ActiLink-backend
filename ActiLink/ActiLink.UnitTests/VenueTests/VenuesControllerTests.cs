d37d5f2fe531f3dce4484ea204e796b26fa1b33c
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-05-10 13:07:47 +0200

    Feature/post venue endpoint (#38)
    * Add VenueService with method for creating new venues
    
    * Add VenuesController with endpoint for creating new venues
    
    * Add migrations after updating BusinessClient
    
    * Correct test method name

---
+++ ActiLink/ActiLink.UnitTests/VenueTests/VenuesControllerTests.cs
@@ -0,0 +1,121 @@
+﻿using System.Security.Claims;
+using ActiLink.Organizers.BusinessClients;
+using ActiLink.Organizers.BusinessClients.DTOs;
+using ActiLink.Shared.Model;
+using ActiLink.Shared.ServiceUtils;
+using ActiLink.Venues;
+using ActiLink.Venues.DTOs;
+using ActiLink.Venues.Service;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ActiLink.UnitTests.VenueTests
+{
+    [TestClass]
+    public class VenuesControllerTests
+    {
+        private Mock<ILogger<VenuesController>> _loggerMock = null!;
+        private Mock<IMapper> _mapperMock = null!;
+        private Mock<IVenueService> _venueServiceMock = null!;
+        private VenuesController _controller = null!;
+
+        const string businessClientId = "TestBCId";
+        readonly Guid venueId = new("030B4A82-1B7C-11CF-9D53-00AA003C9CB6");
+        const string venueName = "Test Venue";
+        const string venueDescription = "Test Description";
+        const string venueAddress = "Test Address";
+        readonly Location venueLocation = new(1.0, 2.0);
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<VenuesController>>();
+            _mapperMock = new Mock<IMapper>();
+            _venueServiceMock = new Mock<IVenueService>();
+
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            _controller = new VenuesController(_venueServiceMock.Object, _loggerMock.Object, _mapperMock.Object)
+            {
+                ControllerContext = controllerContext
+            };
+        }
+
+        [TestMethod]
+        public async Task CreateVenueAsyncByBusinessClient_ValidRequest_ReturnsCreatedResult()
+        {
+            // Given
+            var owner = new BusinessClient("Test Owner", "testowner@email.com", "PL1234567890") { Id = businessClientId };
+            var ownerDto = new VenueOwnerDto(owner.Id, owner.UserName!);
+            var newVenueDto = new NewVenueDto(venueName, venueDescription, venueLocation, venueAddress);
+            var createVenueObject = new CreateVenueObject(businessClientId, venueName, venueDescription, venueLocation, venueAddress);
+            var venue = new Venue(owner, venueName, venueDescription, venueLocation, venueAddress);
+            var expectedVenueDto = new VenueDto(venueId, venueName, venueDescription, venueLocation, venueAddress, ownerDto, []);
+            Utils.SetupVenueId(venue, venueId);
+
+            _mapperMock.Setup(m => m.Map(newVenueDto, It.IsAny<Action<IMappingOperationOptions<object, CreateVenueObject>>>()))
+                .Returns(createVenueObject);
+
+            _mapperMock.Setup(m => m.Map<VenueDto>(venue))
+                .Returns(expectedVenueDto);
+
+            _venueServiceMock.Setup(vs => vs.CreateVenueAsync(createVenueObject))
+                .ReturnsAsync(GenericServiceResult<Venue>.Success(venue));
+
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, businessClientId),
+                new(ClaimTypes.Role, "BusinessClient")
+            };
+
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var principal = new ClaimsPrincipal(identity);
+            _controller.ControllerContext.HttpContext.User = principal;
+
+            // When
+            var result = await _controller.CreateVenueAsync(newVenueDto);
+
+            // Then
+            Assert.IsNotNull(result);
+            var createdAtActionResult = result as CreatedAtActionResult;
+            Assert.IsNotNull(createdAtActionResult);
+            Assert.AreEqual(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
+            Assert.AreEqual(nameof(_controller.GetVenueByIdAsync), createdAtActionResult.ActionName);
+            Assert.IsNotNull(createdAtActionResult.Value);
+            Assert.IsInstanceOfType<VenueDto>(createdAtActionResult.Value);
+            Assert.AreEqual(expectedVenueDto, createdAtActionResult.Value);
+            Assert.IsNotNull(createdAtActionResult.RouteValues);
+            Assert.AreEqual(venueId, createdAtActionResult.RouteValues["id"]);
+        }
+
+        [TestMethod]
+        public async Task CreateVenueAsyncByUser_InvalidRequest_ReturnsForbidden()
+        {
+            // Given
+            var newVenueDto = new NewVenueDto(venueName, venueDescription, venueLocation, venueAddress);
+
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.NameIdentifier, businessClientId),
+                new(ClaimTypes.Role, "User")
+            };
+
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var principal = new ClaimsPrincipal(identity);
+            _controller.ControllerContext.HttpContext.User = principal;
+
+            // When
+            var result = await _controller.CreateVenueAsync(newVenueDto);
+
+            // Then
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType<ForbidResult>(result);
+        }
+    }
+}
---
+++ ActiLink/ActiLink/Migrations/20250510104304_BusinessClient_Venues.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActiLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusinessClient_Venues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
