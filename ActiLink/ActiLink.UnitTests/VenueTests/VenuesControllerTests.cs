5d81926926cbf9454b9e85ed475db3c8ee328933
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-05-11 12:23:28 +0200

    Add get methods in the controller for venues

--- ActiLink/ActiLink.UnitTests/VenueTests/VenuesControllerTests.cs
+++ ActiLink/ActiLink.UnitTests/VenueTests/VenuesControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Security.Claims;
+using ActiLink.Events;
+using ActiLink.Events.DTOs;
 using ActiLink.Organizers.BusinessClients;
 using ActiLink.Organizers.BusinessClients.DTOs;
+using ActiLink.Organizers.DTOs;
 using ActiLink.Shared.Model;
 using ActiLink.Shared.ServiceUtils;
 using ActiLink.Venues;
@@ -12,7 +15,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Moq;
 
 namespace ActiLink.UnitTests.VenueTests
@@ -155,6 +157,151 @@ namespace ActiLink.UnitTests.VenueTests
             // Then
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType<ForbidResult>(result);
+        }
+
+        [TestMethod]
+        public async Task GetVenueByIdAsync_Success_ReturnsOkResult()
+        {
+            // Given
+            var owner = new BusinessClient("Test Owner", "testowner@email.com", "PL1234567890") { Id = businessClientId };
+            var ownerDto = new VenueOwnerDto(owner.Id, owner.UserName!);
+            var organizerDto = new OrganizerDto(owner.Id, owner.UserName!);
+            var venue = new Venue(owner, venueName, venueDescription, venueLocation, venueAddress);
+            var expectedVenueDto = new VenueDto(venueId, venueName, venueDescription, venueLocation, venueAddress, ownerDto, []);
+            Utils.SetupVenueId(venue, venueId);
+            var events = new List<Event>
+            {
+                new(owner, "Test Event", "Test Description", new DateTime(2023, 10, 1), new DateTime(2023, 10, 2), venueLocation, 20m, 1, 10, [], venue),
+                new(owner, "Test Event 2", "Test Description 2", new DateTime(2023, 10, 3), new DateTime(2023, 10, 4), venueLocation, 30m, 1, 10, [], venue)
+            };
+            var eventIds = new List<Guid>
+            {
+                new("030B4A82-1B7C-11CF-9D53-00AA003C9CB6"),
+                new("030B4A82-1B7C-11CF-9D53-00AA003C9CB7")
+            };
+
+            for (int i = 0; i < events.Count; i++)
+                Utils.SetupEventGuid(events[i], eventIds[i]);
+
+            events.ForEach(venue.Events.Add);
+            var expectedEventDtos = events.Select(e => new ReducedEventDto(e.Id, e.Title, organizerDto, e.StartTime)).ToList();
+            expectedVenueDto.Events.AddRange(expectedEventDtos);
+
+            _venueServiceMock.Setup(vs => vs.GetVenueByIdAsync(venueId))
+                .ReturnsAsync(venue);
+
+            _mapperMock.Setup(m => m.Map<VenueDto>(venue))
+                .Returns(expectedVenueDto);
+
+            _mapperMock.Setup(m => m.Map<IEnumerable<ReducedEventDto>>(events))
+                .Returns(expectedEventDtos);
+
+            // When
+            var result = await _controller.GetVenueByIdAsync(venueId);
+
+            // Then
+            Assert.IsNotNull(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.IsNotNull(okResult.Value);
+            Assert.IsInstanceOfType<VenueDto>(okResult.Value);
+            Assert.AreEqual(expectedVenueDto, okResult.Value);
+        }
+
+
+        [TestMethod]
+        public async Task GetVenueByIdAsync_VenueNotFound_ReturnsNotFoundResult()
+        {
+            // Given
+            var nonExistentVenueId = Guid.NewGuid();
+            _venueServiceMock.Setup(vs => vs.GetVenueByIdAsync(nonExistentVenueId))
+                .ReturnsAsync((Venue?)null);
+
+            // When
+            var result = await _controller.GetVenueByIdAsync(nonExistentVenueId);
+
+            // Then
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType<NotFoundResult>(result);
+        }
+
+        [TestMethod]
+        public async Task GetAllVenuesAsync_VenuesFound_ReturnsOkResultWithListWithVenues()
+        {
+            var owner = new BusinessClient("Test Owner", "testowner@email.com", "PL1234567890") { Id = businessClientId };
+            var ownerDto = new VenueOwnerDto(owner.Id, owner.UserName!);
+            var organizerDto = new OrganizerDto(owner.Id, owner.UserName!);
+            var venue1 = new Venue(owner, "Test Venue 1", "Test Description 1", venueLocation, venueAddress);
+            var venue2 = new Venue(owner, "Test Venue 2", "Test Description 2", venueLocation, venueAddress);
+            var expectedVenueDto1 = new VenueDto(venueId, venue1.Name, venue1.Description, venueLocation, venueAddress, ownerDto, []);
+            var expectedVenueDto2 = new VenueDto(venueId, venue2.Name, venue2.Description, venueLocation, venueAddress, ownerDto, []);
+
+            var events = new List<Event>
+            {
+                new(owner, "Test Event", "Test Description", new DateTime(2023, 10, 1), new DateTime(2023, 10, 2), venueLocation, 20m, 1, 10, [], venue1),
+                new(owner, "Test Event 2", "Test Description 2", new DateTime(2023, 10, 3), new DateTime(2023, 10, 4), venueLocation, 30m, 1, 10, [], venue2)
+            };
+            var eventIds = new List<Guid>
+            {
+                new("030B4A82-1B7C-11CF-9D53-00AA003C9CB6"),
+                new("030B4A82-1B7C-11CF-9D53-00AA003C9CB7")
+            };
+            for (int i = 0; i < events.Count; i++)
+                Utils.SetupEventGuid(events[i], eventIds[i]);
+
+            venue1.Events.Add(events[0]);
+            venue2.Events.Add(events[1]);
+
+            var expectedEventDtos = events.Select(e => new ReducedEventDto(e.Id, e.Title, organizerDto, e.StartTime)).ToList();
+            expectedVenueDto1.Events.Add(expectedEventDtos[0]);
+            expectedVenueDto2.Events.Add(expectedEventDtos[1]);
+
+            var venues = new List<Venue> { venue1, venue2 };
+            var expectedVenuesDto = new List<VenueDto> { expectedVenueDto1, expectedVenueDto2 };
+
+            _venueServiceMock.Setup(vs => vs.GetAllVenuesAsync())
+                .ReturnsAsync(venues);
+
+            _mapperMock.Setup(m => m.Map<IEnumerable<VenueDto>>(venues))
+                .Returns(expectedVenuesDto);
+
+            // When
+            var result = await _controller.GetAllVenuesAsync();
+
+            // Then
+            Assert.IsNotNull(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.IsNotNull(okResult.Value);
+            var venueDtos = okResult.Value as IEnumerable<VenueDto>;
+            Assert.IsNotNull(venueDtos);
+            CollectionAssert.AreEqual(expectedVenuesDto, venueDtos.ToList());
+        }
+
+        [TestMethod]
+        public async Task GetAllVenuesAsyns_NoVenuesFound_ReturnsOkResultWithEmptyList()
+        {
+            // Given
+            _venueServiceMock.Setup(vs => vs.GetAllVenuesAsync())
+                .ReturnsAsync([]);
+
+            _mapperMock.Setup(m => m.Map<IEnumerable<VenueDto>>(Enumerable.Empty<Venue>()))
+                .Returns([]);
+
+            // When
+            var result = await _controller.GetAllVenuesAsync();
+
+            // Then
+            Assert.IsNotNull(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(StatusCodes.Status200OK, okResult.StatusCode);
+            Assert.IsNotNull(okResult.Value);
+            var venueDtos = okResult.Value as IEnumerable<VenueDto>;
+            Assert.IsNotNull(venueDtos);
+            Assert.AreEqual(0, venueDtos.Count());
         }
     }
 }
