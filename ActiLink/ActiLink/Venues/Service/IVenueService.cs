44031fdd872c22399c9c9e5efaee2168ace0fa20
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-05-24 22:10:34 +0200

    Feature/delete and edit venues (#43)
    * Add delete and update methods to venue service
    
    * Add delete and update methods to venues controller
    
    * Update method names
    
    * Add authorization to updating venue

--- ActiLink/ActiLink/Venues/Service/IVenueService.cs
+++ ActiLink/ActiLink/Venues/Service/IVenueService.cs
@@ -6,6 +6,8 @@ namespace ActiLink.Venues.Service
     {
         public Task<GenericServiceResult<Venue>> CreateVenueAsync(CreateVenueObject cvo);
         public Task<ServiceResult> DeleteVenueAsync(Venue venueToDelete);
+        public Task<ServiceResult> DeleteVenueByIdAsync(Guid venueId, string requestingUserId);
+        public Task<GenericServiceResult<Venue>> UpdateVenueAsync(Guid venueId, UpdateVenueObject uvo, string requestingUserId);
         public Task<Venue?> GetVenueByIdAsync(Guid venueId);
         public Task<IEnumerable<Venue>> GetAllVenuesAsync();
     }
